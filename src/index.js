a5c19b721d7168b61cb27a0086e29ee46e856b08
Oleksandr Sibirtsev <alsib8@gmail.com>
2023-03-15 10:59:42 +0200

    task-1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener(
+  'input',
+  debounce(e => {
+    const trimOfValue = input.value.trim();
+    cleanHtml();
+
+    if (trimOfValue !== '') {
+      fetchCountries(trimOfValue).then(newData => {
+        if (newData.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (newData.length === 0) {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+        } else if (newData.length >= 2 && newData.length <= 10) {
+          renderCountryList(newData);
+        } else if (newData.length === 1) {
+          renderOneCountry(newData);
+        }
+      });
+    }
+  }, DEBOUNCE_DELAY)
+);
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" hight="20">
+         <p>${country.name.official}</p>
+                </li>`;
+    })
+    .join('');
+
+  countryList.innerHTML = markup;
+}
+
+function renderOneCountry(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${
+        country.name.official
+      }" width="30" hight="20">
+         <p>${country.name.official}</p>
+            <p><b>Capital</b>: ${country.capital}</p>
+            <p><b>Population</b>: ${country.population}</p>
+            <p><b>Languages</b>: ${Object.values(country.languages)} </p>
+                </li>`;
+    })
+    .join('');
+
+  countryList.innerHTML = markup;
+}
+
+function cleanHtml() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
